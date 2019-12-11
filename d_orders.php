9c027a65d06e81075f451a239c2fa0f8a2f5afbc
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-12-11 21:34:58 +0200

    Update d_orders.php

--- d_orders.php
+++ d_orders.php
@@ -2,7 +2,7 @@
 require ("db.php");
 $driver = $_SESSION["driver_id"];
 $query = mysqli_query($con,"SELECT * FROM orders WHERE driver_id = '$driver_id';");  
-
+echo $query;
 if ($query) {
     while ($row = mysqli_fetch_array($query)) {
         $flag[] = $row;
