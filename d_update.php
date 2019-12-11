2c2343ee1bd093174f6b4c7c4d7903b31194898a
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-12-11 21:47:17 +0200

    Update and rename d_update to d_update.php

--- d_update.php
+++ d_update.php
@@ -2,7 +2,7 @@
 require ("db.php");
 $order = $_GET["order_id"];
 $status = $_GET["status"];
-$query = mysqli_query($con,"UPDATE orders SET status = '$status' WHERE driver_id = '$order';");  
+$query = mysqli_query($con,"UPDATE orders SET status = '$status' WHERE id = '$order';");  
 
 $driver = $_GET["driver_id"];
 $query = mysqli_query($con,"SELECT * FROM orders WHERE driver_id = '$driver';");  
