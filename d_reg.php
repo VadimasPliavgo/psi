99416fea8a15c3b2b37f8f90873a1a6b82752792
VadimasPliavgo <31895152+VadimasPliavgo@users.noreply.github.com>
2019-12-11 19:21:42 +0200

    Update d_reg.php

--- d_reg.php
+++ d_reg.php
@@ -7,4 +7,6 @@ $user_pass =$_GET["user_pass"];
 $query = "INSERT into `users` (username, password, email, trn_date, company_name, active, admin, driver)
                         VALUES ('$user_name', '" . md5($user_pass) . "', '$email', '$trn_date', '$company', FALSE, FALSE, TRUE )";
             $result = mysqli_query($con, $query);
+
+echo $query;
             
